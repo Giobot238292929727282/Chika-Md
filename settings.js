a696b17beeb45035e177915b1c7bef398f5eefa4
Giobot238292929727282 <96403857+Giobot238292929727282@users.noreply.github.com>
2022-05-20 15:20:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,22 +22,22 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6282269718172','6282269718172','6282269718172'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6282269718172'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6282269718172' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'GIO BOTZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Alpha-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'GIO BOTZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.youtube = ''
+global.ig = ''
+global.mygc = ''
 global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
+global.email = 'giovaldo776@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
